20991a5797d13393431b6cf1677a8484b14214d2
Val Osip <valosip@gmail.com>
2020-04-05 01:28:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word(string)
+  words = string.split(" ")
+  reversed_words = []
   words.map do |word|
-    word.reverse
+    reversed_words << word.reverse
   end
+  reversed_words.join(" ")
 end
